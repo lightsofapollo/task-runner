86af9a02d61b920a8cca2481cca9adb1b014c44a
James Lal <james@lightsofapollo.com>
2013-09-25 10:32:18 -0700

    handle async (passed) errors

--- test/peform_test.js
+++ test/peform_test.js
@@ -6,6 +6,11 @@ suite('perform', function() {
     return fsPath.join(__dirname, 'fixtures', name);
   }
 
+  function assertError(err, message) {
+    assert.ok(err instanceof Error, 'is an Error');
+    assert.equal(err.message, message, 'error.message');
+  }
+
   test('success', function(done) {
     var input = { itsAnObject: { inAnObject: true }};
 
@@ -19,8 +24,7 @@ suite('perform', function() {
 
   test('sync error (in module)', function(done) {
     perform(fixture('sync_error_load'), {}, function(err) {
-      assert.ok(err, 'has error');
-      assert.equal(err.message, 'xxx', 'err.message');
+      assertError(err, 'xxx');
       // verify we have a nice stack
       assert.ok(
         err.stack.indexOf('sync_error_load.js:1') !== -1,
@@ -32,13 +36,19 @@ suite('perform', function() {
 
   test('sync error (in run)', function(done) {
     perform(fixture('sync_error_run'), {}, function(err) {
-      assert.ok(err, 'has error');
-      assert.equal(err.message, 'yyy', 'err.message');
+      assertError(err, 'yyy');
       // verify we have a nice stack
       assert.ok(
         err.stack.indexOf('sync_error_run.js:2') !== -1,
         err.stack + ' contains correct filename in stack'
       );
+      done();
+    });
+  });
+
+  test('async error (passed)', function(done) {
+    perform(fixture('async_error_pass'), {}, function(err) {
+      assertError(err, 'async');
       done();
     });
   });
